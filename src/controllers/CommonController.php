19294751760dcf61abd489b383acb302630b406f
Smoren <ofigate@gmail.com>
2023-04-14 12:41:41 +0300

    Minor changes

--- src/controllers/CommonController.php
+++ src/controllers/CommonController.php
@@ -3,14 +3,11 @@
 namespace Smoren\Yii2\AccessManager\controllers;
 
 use Smoren\Yii2\AccessManager\interfaces\BehaviorFactoryInterface;
-use Smoren\Yii2\AccessManager\traits\AccessControlTrait;
 use Smoren\Yii2\Auth\controllers\BaseController;
 use Yii;
 
 abstract class CommonController extends BaseController
 {
-    use AccessControlTrait;
-
     public function behaviors(): array
     {
         return array_merge(
--- src/helpers/BehaviorFactory.php
+++ src/helpers/BehaviorFactory.php
@@ -2,12 +2,15 @@
 
 namespace Smoren\Yii2\AccessManager\helpers;
 
+use Smoren\Yii2\AccessManager\filters\AccessControlFilter;
 use Smoren\Yii2\AccessManager\interfaces\BehaviorFactoryInterface;
 
 class BehaviorFactory implements BehaviorFactoryInterface
 {
     public function getBehaviors(): array
     {
-        return [];
+        return [
+            'access' => AccessControlFilter::class,
+        ];
     }
 }
