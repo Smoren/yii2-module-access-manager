4a60577c084074f0612f7240c0a0f3c922486c7a
Smoren <ofigate@gmail.com>
2024-05-17 14:30:47 +0300

    Column sort added to migrations.

--- src/migrations/m211207_132615_create_rule_table.php
+++ src/migrations/m211207_132615_create_rule_table.php
@@ -23,6 +23,7 @@ class m211207_132615_create_rule_table extends Migration
             'alias' => $this->string(255)->notNull(),
             'title' => $this->string(255)->notNull(),
             'is_system' => $this->boolean()->notNull()->defaultValue(false),
+            'sort' => $this->integer()->notNull()->defaultValue(0),
             'extra' => $this->json(),
             'created_at' => $this->createdAt(),
             'updated_at' => $this->updatedAt(),
@@ -31,6 +32,7 @@ class m211207_132615_create_rule_table extends Migration
         $this->createIndex("idx-{$tablePrefix}_rule-alias", "{$tablePrefix}_rule", 'alias', true);
         $this->createIndex("idx-{$tablePrefix}_rule-title", "{$tablePrefix}_rule", 'title');
         $this->createIndex("idx-{$tablePrefix}_rule-is_system", "{$tablePrefix}_rule", 'is_system');
+        $this->createIndex("idx-{$tablePrefix}_rule-sort", "{$tablePrefix}_rule", 'sort');
     }
 
     /**
