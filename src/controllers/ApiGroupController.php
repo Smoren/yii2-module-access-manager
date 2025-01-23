8cf8863867286af89c42e9c84dcdf5e93a263754
a.strahov <astrahov90@gmail.com>
2025-01-23 10:19:09 +0300

    Api Group GetTree method added

--- src/controllers/ApiGroupController.php
+++ src/controllers/ApiGroupController.php
@@ -2,6 +2,7 @@
 
 namespace Smoren\Yii2\AccessManager\controllers;
 
+use Smoren\Helpers\TreeHelper;
 use Smoren\Yii2\AccessManager\forms\api_group\ApiGroupCreateForm;
 use Smoren\Yii2\AccessManager\forms\api_group\ApiGroupFilterForm;
 use Smoren\Yii2\AccessManager\forms\api_group\ApiGroupUpdateForm;
@@ -9,10 +10,48 @@ use Smoren\Yii2\AccessManager\models\ApiGroup;
 use Smoren\Yii2\AccessManager\models\query\ApiGroupQuery;
 use Smoren\Yii2\ActiveRecordExplicit\models\ActiveQuery;
 use Smoren\Yii2\ActiveRecordExplicit\models\Model;
+use Smoren\Yii2\Auth\controllers\RestControllerTrait;
+use Smoren\Yii2\Auth\exceptions\ApiException;
+use Smoren\Yii2\Auth\structs\StatusCode;
 use Yii;
 
 class ApiGroupController extends CommonRestController
 {
+    use RestControllerTrait {
+        getRules as public static getRulesTrait;
+    }
+
+    public static function getRules(string $apiPath, string $controllerPath, string $itemIdValidationRegexp): array
+    {
+        $rules = self::getRulesTrait($apiPath, $controllerPath, $itemIdValidationRegexp);
+
+        return array_merge(
+            $rules,
+            [
+                /**
+                 * API for getting tree collection
+                 * @see ApiGroupController::actionTree()
+                 */
+                "GET {$apiPath}" => "{$controllerPath}/collection",
+            ]
+        );
+    }
+
+    /**
+     * Action for getting tree collection
+     * @return array
+     * @throws ApiException
+     */
+    public function actionTree(): array
+    {
+        $this->checkAccess(__FUNCTION__);
+        Yii::$app->response->statusCode = StatusCode::OK;
+
+        $list = $this->getCollectionQuery()->all();
+
+        return TreeHelper::fromList($list);
+    }
+
     /**
      * @inheritDoc
      * @return ApiGroupCreateForm
